56a0052738cae0960d717bd079878aa5fd0d9527
NiveshAgrawal <niveshagrawal104@gmail.com>
2022-09-08 11:57:10 +0530

    i added some files

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    printf("hello wolrd \n");
+    printf("hello wolrd \n");
+    printf("hello wolrd \n");
+    printf("hello wolrd \n");
+    printf("hello wolrd \n");
+    printf("hello wolrd \n");
+    printf("hello wolrd \n");
+    return 0;
+}
+
